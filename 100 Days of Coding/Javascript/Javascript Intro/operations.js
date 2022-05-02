0637b626fc787da7703adc69948134f484a129ca
Davon20 <davonalbury@outlook.com>
2022-05-02 11:13:02 -0400

    Added Operations Example

---
+++ 100 Days of Coding/Javascript/Javascript Intro/operations.js
@@ -0,0 +1,16 @@
+console.log(10 + 4);
+console.log(10 - 4);
+console.log(10 * 4);
+console.log(10 / 4);
+console.log(10 % 4); // 10 / 4 => 2 * 4 => Remainder: 2
+console.log(10 % 3); // Expected Remainder: 1
+
+console.log(10 + 3 - 5) * 10; 
+console.log("Davon" + " " + "Albury");
+
+let userName = "Davon";
+console.log(userName.length);
+console.log(userName.toUpperCase());
+
+let hobbies = ["Sports", "Cooking"];
+console.log(hobbies.length);
