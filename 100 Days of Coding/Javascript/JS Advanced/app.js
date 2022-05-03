515b4a008e9d32718cd33628349fc01e3bf6d2b4
Davon20 <davonalbury@outlook.com>
2022-05-02 22:03:58 -0400

    Added How To Select DOM Elements Notes

--- 100 Days of Coding/Javascript/JS Advanced/app.js
+++ 100 Days of Coding/Javascript/JS Advanced/app.js
@@ -1,1 +1,5 @@
 document.body.children[1].children[0].href = "https://google.com";
+/* select DOM elements through nest */
+
+document.getElementById("paragraph");
+/* select DOM elements through querying for id or class */
