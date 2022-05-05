41e6ba90ebd2813fa6622568a7f679e6ee20fc00
Davon20 <davonalbury@outlook.com>
2022-05-05 18:50:55 -0400

    Character Reader

---
+++ 100 Days of Coding/Javascript/JS Advanced/demo.js
@@ -0,0 +1,21 @@
+//These variables defined the elements being called by the function
+let productNameInputElement = document.getElementById("product-name");
+let remainingCharsElement = document.getElementById("remaining-chars");
+
+let maxAllowedChars = productNameInputElement.maxLength;
+console.log(maxAllowedChars);
+
+//event grants access to the target of the element
+function updateRemainingCharacters(event){
+    //This variable reaches out to the targets
+    let enteredText = event.target.value;
+    //Finds out how long the user input is
+    let enteredTextLength = enteredText.length;
+    //Calculates the remaining characters
+
+    let remainingCharacters = maxAllowedChars - enteredTextLength;
+
+    remainingCharsElement.textContent = remainingCharacters;
+}
+//Listens to the user input
+productNameInputElement.addEventListener("input", updateRemainingCharacters);
