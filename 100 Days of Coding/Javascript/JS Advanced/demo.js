dc8cd0e66116ed9487c89e0de77e65c4d73bbdfa
Davon20 <davonalbury@outlook.com>
2022-05-06 11:27:33 -0400

    Intro to Exercise

--- 100 Days of Coding/Javascript/JS Advanced/demo.js
+++ 100 Days of Coding/Javascript/JS Advanced/demo.js
@@ -1,20 +1,23 @@
 //These variables defined the elements being called by the function
-let productNameInputElement = document.getElementById("product-name");
-let remainingCharsElement = document.getElementById("remaining-chars");
+//const is a variable that cannot be overwritten
+const productNameInputElement = document.getElementById("product-name");
+const remainingCharsElement = document.getElementById("remaining-chars");
 
+//Defines the variable that will serve as the dynamic number
 let maxAllowedChars = productNameInputElement.maxLength;
-console.log(maxAllowedChars);
 
 //event grants access to the target of the element
 function updateRemainingCharacters(event){
     //This variable reaches out to the targets
     let enteredText = event.target.value;
+    
     //Finds out how long the user input is
     let enteredTextLength = enteredText.length;
-    //Calculates the remaining characters
-
+    
+    //Calculates the remaining characters dynamically rather than hardcoding the int 60
     let remainingCharacters = maxAllowedChars - enteredTextLength;
 
+    //Updates the value of the remainingCharacters
     remainingCharsElement.textContent = remainingCharacters;
 }
 //Listens to the user input
