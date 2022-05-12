c1464b2f40839290a89cc3d062baddbbfef60d13
Davon20 <davonalbury@outlook.com>
2022-05-12 18:37:18 -0400

    Tic Tac Toe Strucutre, Game Implementation Tomorrow

---
+++ 100 Days of Coding/Javascript/Tic_Tac_Toe/scripts/app.js
@@ -0,0 +1,26 @@
+//Initializes Constants that will implemented by the other scripts
+
+let editedPlayer = 0;
+
+const players = [
+    {name: "",
+    symbol: "X"}, 
+    {name: "",
+    symbol: "O"}];
+
+const playerConfigOverlayElement = document.getElementById("config-overlay");
+const backdropElement = document.getElementById("backdrop");
+const formElement = document.querySelector("form");
+const errorsOutputElement = document.getElementById("config-errors");
+
+const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
+const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
+const cancelConfigButnElement = document.getElementById("cancel-config-btn");
+
+editPlayer1BtnElement.addEventListener("click", openPlayerConfig)
+editPlayer2BtnElement.addEventListener("click", openPlayerConfig)
+
+cancelConfigButnElement.addEventListener("click", closePlayerConfig)
+backdropElement.addEventListener("click", closePlayerConfig)
+
+formElement.addEventListener("submit", savePlayerConfig)
---
+++ 100 Days of Coding/Javascript/Tic_Tac_Toe/scripts/config.js
@@ -0,0 +1,37 @@
+//Stores The Functionality of JS Code
+function openPlayerConfig(event) {
+    editedPlayer = +event.target.dataset.playerid;
+    playerConfigOverlayElement.style.display = "block";
+    backdropElement.style.display = "block";
+}
+
+function closePlayerConfig() {
+    playerConfigOverlayElement.style.display = "none";
+    backdropElement.style.display = "none";
+    formElement.firstElementChild.classList.remove("error");
+    errorsOutputElement.textContent = "";
+    formElement.firstElementChild.lastElementChild.value = "";
+}
+
+function savePlayerConfig(event){
+    /* This is being used to keep the browser from sending the info of the 
+    user to a server that isn't currently present as all code is only being
+    run between HTML, CSS, & JS */
+    event.preventDefault();
+    //Form Data object handles the browser traffic of storing user data to just the browser
+    const formData = new FormData(event.target);
+    //.get uses the string value of the username to save that value
+    const enteredPlayerName = formData.get("playername").trim();
+
+    if(!enteredPlayerName){
+        event.target.firstElementChild.classList.add("error");
+        errorsOutputElement.textContent = "Only enter a valid name!";
+        return;
+    }
+    const updatedPlayerDataElement = document.getElementById("player-" + editedPlayer + "-data");
+    updatedPlayerDataElement.children[1].textContent = enteredPlayerName;
+    //dynamically accesses the current player where a If-Else statement would be needed
+    players[editedPlayer -1].name = enteredPlayerName;
+    //manually closes after the submit/cancel buttons are clicked
+    closePlayerConfig();
+}
